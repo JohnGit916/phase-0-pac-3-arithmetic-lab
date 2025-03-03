ebc8c2b568349383f9253122dfc1041308761a19
John <john.gitonga@student.moringaschool.com>
2025-03-03 10:17:05 +0300

    updated

---
+++ index.js
@@ -0,0 +1,41 @@
+1 + 80; //=> 81
+60 - 40; //=> 20
+2 * 3.4; //=> 6.8
+5.0 / 2.5; //=> 2
+
+function add (a, b) {
+  1 + 80;
+  return a + b;
+}
+
+function subtract (a, b) {
+  60 - 40;
+  return a - b;
+}
+
+function multiply (a, b) {
+  2 * 3.4 ;
+  return a * b;
+}
+
+function divide (a, b) {
+  5.0 / 2.5;
+  return a / b;
+}
+
+ 
+function increment (n) {
+  return n + 1 ;
+}
+
+function decrement (n) {
+  return n - 1 ;
+}
+
+function makeInt(n) {
+  return parseInt(n, 10);
+}
+
+function preserveDecimal (n) {
+  return parseFloat(n);
+}
